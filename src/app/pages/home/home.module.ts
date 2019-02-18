a08f48f60136c634423dedf620f421b1f393536c
zhazihong <1982780677@qq.com>
2019-02-18 12:20:29 +0800

    聊天demo

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,10 +1,3 @@
-/**
- * Created by JetBrains WebStorm.
- * Author: yoon
- * Date: 17-11-27
- * Time: 下午3:23
- * Desc:
- */
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
